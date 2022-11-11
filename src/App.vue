6c626b19fd613260b55d98ec350b9b2cd8030ea4
DESKTOP-TOJLL8R\10Administrator <Josip.Maric@tri-m.hr>
2022-11-11 14:14:40 +0100

    Added auto login

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,40 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
 import NavBarComponent from '@/components/showComponents/NavBarComponent.vue'
+import http from '@/http-common'
 
 @Options({
   components: {
     NavBarComponent
   }
 })
-export default class App extends Vue {}
+export default class Home extends Vue {
+  created (): void {
+    const temp = location.hash.substr(1)
+    if (temp !== '' && localStorage.getItem('access_token') === '') {
+      const value = JSON.parse(atob(temp))
+      console.log(value.access_token)
+      localStorage.setItem('access_token', value.access_token)
+      history.pushState('', document.title, window.location.pathname + window.location.search)
+      location.reload()
+      // alert('yes')
+      // this.$router.go(0)
+    } else {
+      this.checkBearer()
+    }
+  }
+
+  async checkBearer () : Promise<void> {
+    await http.get('http://blog.test/api/form').then(response => {
+      // success
+    }, response => {
+      // error
+      const temp = String(response)
+      if (temp.includes('code 401')) {
+        localStorage.setItem('access_token', '')
+        window.location.href = 'http://blog.test/oauth/redirect'
+      }
+    })
+  }
+}
 </script>
