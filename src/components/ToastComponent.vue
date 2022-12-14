9b418e0bfe75ea0c0f3591b585e2d71bfc09fab7
CroBanana <danko.salopek1@gmail.com>
2022-12-14 09:48:39 +0100

    Toast od bootstrapa
    Problemi koji postoje, koje nemam vise mentalne snage popravit, i koje se ne zelim više dirat:
    1. Ne mijenja se  title i message od toasta
    2. Kako mijenjat boju od toasta
    
    Funkcija u kojoj sam to pokušavao se nalazi u views/PermissionTree.vue   naziv      "toast"

---
+++ src/components/ToastComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="position-fixed top-0 end-0 p-3" style="z-index: 11">
+    <div id="liveToast" :class='customClass' role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-header">
+        <strong class="me-auto">{{ title }}</strong>
+        <small>upravo izvršeno</small>
+        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+      <div class="toast-body">
+        {{ message }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+@Options({
+  props: {
+    title: String,
+    message: String,
+    customClass: {
+      type: String,
+      default: 'toast hide'
+    }
+  }
+})
+export default class ShowInfo extends Vue {
+  title!: string
+  message!: string
+  customClass!: string
+}
+</script>
