2cee5df0a2f1a187038701aafd67efaa3c36ea87
DESKTOP-TOJLL8R\10Administrator <Josip.Maric@tri-m.hr>
2023-01-13 10:36:19 +0100

    fix

--- src/components/showComponents/NavBarComponent.vue
+++ src/components/showComponents/NavBarComponent.vue
@@ -23,19 +23,17 @@
             </div>
           </div>
         </div>
-      <!--ul class="d-flex">
-        <li class="nav-item">
-        <ul class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle boja" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            User name
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <router-link to="/account" class="dropdown-item">Profile</router-link>
-            <li><button class="dropdown-item" type="button" v-on:click="logout()">Log out</button></li>
-          </ul>
+        <ul class="d-flex">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle boja" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              User name
+            </a>
+            <ul class="dropdown-menu dropdown-content">
+              <router-link to="/account" class="dropdown-item">Profile</router-link>
+              <li><button class="dropdown-item" type="button" v-on:click="logout()">Log out</button></li>
+            </ul>
+          </li>
         </ul>
-        </ul-->
       </div>
     </div>
   </nav>
